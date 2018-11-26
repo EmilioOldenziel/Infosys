430cb415e95f88c34f1dac3e5ae4cf038540f0c1
erikbijl <bijlerik@hotmail.com>
2018-11-26 22:26:56 +0100

    created triggers and functions

--- Assignment2/schema.sql
+++ Assignment2/schema.sql
@@ -1,8 +1,6 @@
-/*DROP TABLE customer; 
-DROP TABLE artist; 
-DROP TABLE purchase;
-*/
-CREATE TABLE customer2 (
+/* create tables customer, artist and purchase */
+
+CREATE TABLE customer (
 	cust_no int,
 	cust_name varchar(50),
 	cust_addr varchar(200),
@@ -10,7 +8,7 @@ CREATE TABLE customer2 (
 	CONSTRAINT customers_pk PRIMARY KEY (cust_no)
  );
 
-/*CREATE TABLE artist (
+CREATE TABLE artist (
 	art_id int,
 	art_name varchar(50),
 	art_code varchar(200),
@@ -20,28 +18,67 @@ CREATE TABLE customer2 (
 
 CREATE TABLE purchase (
 	cust_no int,
-	art_id INT,
+	art_id int,
 	pur_date DATE,
 	price int,
 	CONSTRAINT purchases_pk PRIMARY KEY (cust_no, art_id)
  );
-*/
 
-/*CREATE OR REPLACE FUNCTION customer_to_uppercase() RETURNS TRIGGER AS $$
-    BEGIN
-        UPDATE customer SET cust_name = UPPER(cust_name);
-	    RETURN NEW;
-    END;
-$$ LANGUAGE plpgsql;
+/* function that changes a name of customer to its uppercase */
+CREATE OR REPLACE FUNCTION cust_uppercase()
+RETURNS TRIGGER AS 
+$BODY$
+BEGIN
+	UPDATE customer SET cust_name = UPPER(cust_name);
+	RETURN NEW;
+END;
+$BODY$
+language plpgsql;
 
-*/
+/* function that changes a name of artist to its uppercase */
+CREATE OR REPLACE FUNCTION art_uppercase()
+RETURNS TRIGGER AS 
+$BODY$
+BEGIN
+	UPDATE artist SET art_name = UPPER(art_name);
+	RETURN NEW;
+END;
+$BODY$
+language plpgsql;
 
 
-create function cust2upper() returns trigger as $$
-begin
-    new.cust_name := old.cust_name;
-    return new;
-end
-$$ language plpgsql;
+CREATE OR REPLACE FUNCTION price_pos() 
+RETURNS TRIGGER AS 
+$BODY$
+BEGIN 
+  IF NEW.price <= 0 THEN 
+    RAISE EXCEPTION 'price is not positive'; 
+  END IF; 
+  return new; 
+END;
+$BODY$
+language plpgsql;
 
-create trigger mytrigger before insert on customer2 for each row execute procedure cust2upper();
+/* trigger that is triggered after each insert or update on customer */
+CREATE TRIGGER cust_upper
+AFTER INSERT OR UPDATE 
+ON customer 
+FOR EACH ROW 
+WHEN (pg_trigger_depth() = 0)
+EXECUTE PROCEDURE cust_uppercase();
+
+/* trigger that is triggered after each insert or update on artist */
+CREATE TRIGGER art_upper
+AFTER INSERT OR UPDATE 
+ON artist 
+FOR EACH ROW 
+WHEN (pg_trigger_depth() = 0)
+EXECUTE PROCEDURE art_uppercase();
+
+/* trigger that is raises error when price is not greater than 0 */
+CREATE TRIGGER price_check
+AFTER INSERT OR UPDATE 
+ON purchase 
+FOR EACH ROW 
+WHEN (pg_trigger_depth() = 0)
+EXECUTE PROCEDURE price_pos();
